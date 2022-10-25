7a99c54a46ba8fc326fe6828a82f978f8abf4ecb
Julia <kico1.14.5@gmail.com>
2022-10-25 19:23:20 -0400

    For loops and range

---
+++ diff_for_loops_range.py
@@ -0,0 +1,24 @@
+s = "Sarah cutie" # 11 letters
+
+for a in s:
+    print(a, end=" ") # S a r a h   c u t i e
+    
+print()
+    
+for b in range(len(s)): # 0 1 2 3 4 5 6 7 8 9 10 
+    print(b, end=" ")
+    
+print()
+    
+for c in range(0,5): # 0 1 2 3 4
+    print(c, end=" ")
+    
+print()
+    
+for d in range(5): # 0 1 2 3 4
+    print(d, end=" ")
+    
+print()
+    
+for e in range(0,10,2): # 0 2 4 6 8
+    print(e, end=" ")
